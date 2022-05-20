eb7ba69f91395e6731e32b54389142b560b4fe83
smile <s930175@gmail.com>
2022-05-20 09:54:12 +0800

    毀滅sessiion

--- controllers/auth.js
+++ controllers/auth.js
@@ -39,9 +39,15 @@ const postLogin = (req, res) => {
 };
 
 const postLogout = (req, res) => {
-    req.session.isLogin = false;
-    // TODO: 實作 logout 機制
-    res.redirect('/login')
+    // req.session.isLogin = false;
+    // // TODO: 實作 logout 機制
+    // res.redirect('/login')
+
+    //登出時毀滅session
+    req.session.destroy((err) => {
+        console.log('session destroy() error: ', err);
+        res.redirect('/login');
+    });
 }
 
 module.exports = {
